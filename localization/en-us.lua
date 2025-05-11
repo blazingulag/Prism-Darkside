6a0d147ca27d4367b0374d257a784904a65247b0
blazingulag <k8lorenzo@gmail.com>
2025-05-11 12:22:19 +0200

    minor spelling mistake

--- localization/en-us.lua
+++ localization/en-us.lua
@@ -17,7 +17,7 @@ return {
             },
             j_pridark_surprise_test = {
                 name = "Surprise Test",
-                text = {"Palyed cards with rank {C:attention}X{}",
+                text = {"Played cards with rank {C:attention}X{}",
                     "give {X:red,C:white}X#1#{} Mult when scored,",
                     "where {C:attention}X{} is any valid solution",
                     "of: {C:attention}#2#",
